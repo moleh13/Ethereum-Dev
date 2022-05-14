2b1bfca71c2a6d22754655ad8d5e440eb9b3e636
moleh13 <molehann@gmail.com>
2022-05-14 21:56:22 +0300

    vault is working

--- depositTest/contracts/Vault_3.sol
+++ depositTest/contracts/Vault_3.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract vault {
+contract vault_3 {
     mapping (address => uint) vaultBalances;
 
     function deposit(uint _amount) public payable {
-        vaultBalances[msg.sender] += _amount;
+        require(msg.value == _amount);
+        vaultBalances[msg.sender] += msg.value;
+    }
+
+    receive() external payable {}
+
+    function getBalance() public view returns(uint){
+        return vaultBalances[msg.sender];
     }
 
     function withdraw(uint _amount) public payable {
---
+++ depositTest/migrations/5_vault_4_migration.js
@@ -0,0 +1,5 @@
+const Vault_4 = artifacts.require("Vault_4");
+
+module.exports = function (deployer) {
+  deployer.deploy(Vault_4);
+};
