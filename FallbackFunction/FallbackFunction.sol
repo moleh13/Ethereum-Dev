8ad421ffd40621dfc0eeed764f7fc418252462d6
moleh13 <molehann@gmail.com>
2022-05-15 21:45:05 +0300

    receive function added

--- FallbackFunction/FallbackFunction.sol
+++ FallbackFunction/FallbackFunction.sol
@@ -8,6 +8,8 @@ contract Fallback {
         emit Log(gasleft());
     }
 
+    receive() external payable {}
+
     function getBalance() public view returns(uint) {
         return(address(this).balance);
     }
