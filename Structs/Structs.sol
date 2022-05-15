c7873e51dd52840db5a26749a63addb72594aaac
moleh13 <molehann@gmail.com>
2022-05-15 16:39:03 +0300

    structs

---
+++ Structs/Structs.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Todos {
+
+    struct Todo {
+        string text;
+        bool completed;
+    }
+
+    Todo[] public todos;
+
+    function create(string memory _text) public {
+        todos.push(Todo(_text, false));
+
+       /* Todo({
+            text: _text,
+            completed: false
+        })
+
+        Todo memory todo;
+        todo.text = _text;
+        // todo.completed = false; */
+    }
+
+    function get(uint _index) public view returns (string memory, bool) {
+        Todo storage todo = todos[_index];
+        return(todo.text, todo.completed);
+    }
+
+    function get_2(uint _index) public view returns(string memory text, bool completed) {
+        Todo storage todo = todos[_index];
+        return(todo.text, todo.completed);
+    }
+
+    function updateText(uint _index, string memory _text) public {
+        Todo storage todo = todos[_index];
+        todo.text = _text;
+    }
+
+    function toggleCompleted(uint _index, string memory _text) public {
+        Todo storage todo = todos[_index];
+        todo.completed = !todo.completed;
+    }
+}
