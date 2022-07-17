3a3f803fe2f3d3d49760870895c7a1659fba59f5
moleh13 <molehann@gmail.com>
2022-07-17 23:35:36 +0300

    default values

---
+++ SolidityLessons/DefaultValues/DefaultValues.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+contract DefaultValues {
+    bool public b;    // false
+    uint public u;    // 0
+    int public i;     // 0
+    address public a; // 0x0000000000000000000000000000000000000000
+    bytes public b32; // 0x0000000000000000000000000000000000000000000000000000000000000000
+}
