9e1b2058c2f445c646eb44040b3c82b24bdacca6
moleh13 <molehann@gmail.com>
2022-07-21 07:17:05 +0300

    enum

--- SolidityLessons/Enum2/Enum.sol
+++ SolidityLessons/Enum2/Enum.sol
@@ -17,4 +17,22 @@ contract Enum {
         address buyer;
         Status status;
     }
+
+    Order[] public orders;
+
+    function get() public view returns (Status) {
+        return status;
+    }
+
+    function set(Status _status) external {
+        status = _status;
+    }
+
+    function ship() external {
+        status = Status.Shipped;
+    }
+
+    function reset() external {
+        delete status;
+    }
 }
