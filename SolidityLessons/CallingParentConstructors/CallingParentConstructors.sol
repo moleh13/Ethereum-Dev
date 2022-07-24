d70231397dedd95d9377a965af566deeca83a95e
moleh13 <molehann@gmail.com>
2022-07-24 15:23:12 +0300

    calling parent constructor

--- SolidityLessons/CallingParentConstructors/CallingParentConstructors.sol
+++ SolidityLessons/CallingParentConstructors/CallingParentConstructors.sol
@@ -36,3 +36,43 @@ contract VV is S("s"), T {
     }
 }
 
+// Order of execution
+// 1. S
+// 2. T
+// 3. V0
+contract V0 is S, T {
+    constructor(string memory _name, string memory _text) S(_text) T(_name) {
+
+    }
+}
+
+// Order of execution
+// 1. S
+// 2. T
+// 3. V1
+contract V1 is S, T {
+    constructor(string memory _name, string memory _text) T(_text) S(_text) {
+
+    }
+}
+
+// Order of execution
+// 1. T
+// 2. S
+// 3. V2
+contract V2 is T, S {
+    constructor(string memory _name, string memory _text) T(_text) S(_name) {
+
+    }
+}
+
+// Order of execution
+// 1. T
+// 2. S
+// 3. V3
+contract V3 is T, S {
+    constructor(string memory _name, string memory _text) S(_name) T(_text) {
+
+    }
+}
+
